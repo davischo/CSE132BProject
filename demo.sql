b76c6d67942d640c4b12731202c7b4c66d723d6e
Jenny Nguyen <ttn053@ucsd.edu>
2017-05-30 07:34:46 -0700

    insertion

---
+++ demo.sql
@@ -0,0 +1,172 @@
+#CLASSES AND SECTIONS
+Insert into classes(course_name,title,quarter,year) values('CSE 8A','Introduction to CS: Java','FA',2014);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,9,'Justin Bieber');
+Insert into classes(course_name,title,quarter,year) values('CSE 8A','Introduction to CS: Java','SP',2015);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,11,'Kelly Clarlson');
+Insert into classes(course_name,title,quarter,year) values('CSE 8A','Introduction to CS: Java','FA',2015);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,13,'Selena Gomez');
+Insert into classes(course_name,title,quarter,year) values('CSE 8A','Introduction to CS: Java','FA',2016);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,22,'Selena Gomez');
+Insert into classes(course_name,title,quarter,year) values('CSE 8A','Introduction to CS: Java','WI',2016);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,23,'Selena Gomez');
+Insert into classes(course_name,title,quarter,year) values('CSE 8A','Introduction to CS: Java','WI',2017);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,24,'Selena Gomez');
+Insert into classes(course_name,title,quarter,year,scheduled_fac) values('CSE 8A','Introduction to CS: Java','SP',2018,'Selena Gomez');
+
+Insert into classes(course_name,title,quarter,year) values('CSE105','Intro to Theory','WI',2015);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,14,'Taylor Swift');
+Insert into classes(course_name,title,quarter,year) values('CSE105','Intro to Theory','WI',2016);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,20,'Taylor Swift');
+Insert into classes(course_name,title,quarter,year,scheduled_fac) values('CSE105','Intro to Theory','FA',2017,'Taylor Swift');
+
+
+Insert into classes(course_name,title,quarter,year) values('CSE250A','Probability Reasoning','FA',2014);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,15,'Bjork');
+Insert into classes(course_name,title,quarter,year) values('CSE250A','Probability Reasoning','FA',2015);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,16,'Bjork');
+Insert into classes(course_name,title,quarter,year) values('CSE250A','Probability Reasoning','WI',2015);
+Insert into classes(course_name,title,quarter,year) values('CSE250A','Probability Reasoning','FA',2016);
+Insert into classes(course_name,title,quarter,year,scheduled_fac) values('CSE250A','Probability Reasoning','SP',2018,'Bjork');
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,17,'Bjork');
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,18,'Bjork');
+
+Insert into classes(course_name,title,quarter,year) values('CSE250B','Machine Learning','WI',2015);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,26);
+Insert into classes(course_name,title,quarter,year) values('CSE250B','Machine Learning','WI',2016);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,27,'Justin Bieber');
+Insert into classes(course_name,title,quarter,year) values('CSE250B','Machine Learning','FA',2015);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,27);
+Insert into classes(course_name,title,quarter,year) values('CSE250B','Machine Learning','FA',2018);
+
+Insert into classes(course_name,title,quarter,year) values('CSE255','Data Mining And Predictive Analytics','FA',2015);
+Insert into classes(course_name,title,quarter,year) values('CSE255','Data Mining And Predictive Analytics','WI',2016);
+Insert into classes(course_name,title,quarter,year) values('CSE255','Data Mining And Predictive Analytics','WI',2018);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,30,'Adam Levine');
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,31);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,32);
+
+Insert into classes(course_name,title,quarter,year) values('CSE232A','Databases','FA',2015);
+Insert into classes(course_name,title,quarter,year) values('CSE232A','Databases','SP',2018);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,33,'Kelly Clarlson');
+
+Insert into classes(course_name,title,quarter,year) values('CSE221','Operating Systems','SP',2015);
+Insert into classes(course_name,title,quarter,year) values('CSE221','Operating Systems','WI',2016);
+Insert into classes(course_name,title,quarter,year) values('CSE221','Operating Systems','FA',2017);
+
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,35);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,37);
+
+Insert into classes(course_name,title,quarter,year) values('MAE107','Computational Methods','SP',2015);
+Insert into classes(course_name,title,quarter,year) values('MAE107','Computational Methods','SP',2016);
+Insert into classes(course_name,title,quarter,year) values('MAE107','Computational Methods','SP',2018);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,39,'Bjork');
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,38);
+
+Insert into classes(course_name,title,quarter,year) values('MAE108','Probability And Statistics','FA',2014);
+Insert into classes(course_name,title,quarter,year) values('MAE108','Probability And Statistics','WI',2015);
+Insert into classes(course_name,title,quarter,year) values('MAE108','Probability And Statistics','WI',2016);
+Insert into classes(course_name,title,quarter,year) values('MAE108','Probability And Statistics','WI',2017);
+Insert into classes(course_name,title,quarter,year) values('MAE108','Probability And Statistics','FA',2018);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,44,'Selena Gomez');
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,41);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,42);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,43);
+
+Insert into classes(course_name,title,quarter,year) values('PHIL10','Intro to Logic','FA',2015);
+Insert into classes(course_name,title,quarter,year) values('PHIL10','Intro to Logic','FA',2016);
+Insert into classes(course_name,title,quarter,year) values('PHIL10','Intro to Logic','WI',2018);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,47,'Bjork');
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,46);
+
+Insert into classes(course_name,title,quarter,year) values('PHIL12','Scientific Reasoning','WI',2016);
+Insert into classes(course_name,title,quarter,year) values('PHIL12','Scientific Reasoning','SP',2018);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,48);
+
+Insert into classes(course_name,title,quarter,year) values('PHIL165','Freedom, Equality, and The Law','SP',2015);
+Insert into classes(course_name,title,quarter,year) values('PHIL165','Freedom, Equality, and The Law','FA',2015);
+Insert into classes(course_name,title,quarter,year) values('PHIL165','Freedom, Equality, and The Law','WI',2016);
+Insert into classes(course_name,title,quarter,year) values('PHIL165','Freedom, Equality, and The Law','SP',2016);
+Insert into classes(course_name,title,quarter,year) values('PHIL165','Freedom, Equality, and The Law','FA',2016);
+Insert into classes(course_name,title,quarter,year) values('PHIL165','Freedom, Equality, and The Law','SP',2018);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,51);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,52);
+Insert into sections(sec_id,enr_limit,class_id) values(11,90,53);
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,54,'Flo Rida');
+Insert into sections(sec_id,enr_limit,class_id,taught_by) values(11,90,55,'Adam Levine');
+
+
+
+#ENROLLMENT 
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(4,9,'FA',2014,4,'Letter Grade', 'A-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(6,9,'FA',2014,4,'Letter Grade', 'B+');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(5,11,'SP',2015,4,'Letter Grade', 'C-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(7,13,'FA',2015,4,'Letter Grade', 'A-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(8,13,'FA',2015,4,'Letter Grade', 'B');
+
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(4,14,'WI',2015,4,'Letter Grade', 'A-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(8,14,'WI',2015,4,'Letter Grade', 'B+');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(7,14,'WI',2015,4,'Letter Grade', 'C');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(20,15,'FA',2014,4,'Letter Grade', 'C');
+
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(26,16,'FA',2015,4,'Letter Grade', 'B+');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(22,16,'FA',2015,4,'Letter Grade', 'D');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(23,16,'FA',2015,4,'Letter Grade', 'F');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(21,26,'WI',2015,4,'Letter Grade', 'A');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(23,26,'WI',2015,4,'Letter Grade', 'A');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(24,30,'FA',2015,4,'Letter Grade', 'B-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(22,30,'FA',2015,4,'Letter Grade', 'B');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(25,30,'FA',2015,4,'Letter Grade', 'F');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(21,33,'FA',2015,4,'Letter Grade', 'A-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(26,35,'SP',2015,4,'Letter Grade', 'A');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(24,35,'SP',2015,4,'Letter Grade', 'A');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(14,38,'SP',2015,4,'Letter Grade', 'B+');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(12,41,'FA',2014,2,'Letter Grade', 'B-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(11,41,'FA',2014,2,'Letter Grade', 'A-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(10,42,'WI',2015,2,'Letter Grade', 'B');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(14,42,'WI',2015,2,'Letter Grade', 'B+');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(15,46,'FA',2015,4,'Letter Grade', 'A');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(16,46,'FA',2015,4,'Letter Grade', 'A');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(17,46,'FA',2015,4,'Letter Grade', 'C-');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(18,46,'FA',2015,4,'Letter Grade', 'C+');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(19,51,'SP',2015,2,'Letter Grade', 'F');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(16,51,'SP',2015,2,'Letter Grade', 'D');
+insert into enrollment(s_id,class_id, quarter, year, units, grade_opt, grade)
+values(15,52,'FA',2015,2,'Letter Grade', 'A-');
+
+#QUERY USED FOR ENROLLMENT
+-- select st.id, cs.course_name,cs.class_id, sec_id 
+-- from students st, classes cs, sections s
+-- where first = 'Vikram' and cs.course_name='PHIL165' 
+-- and cs.quarter='FA' and cs.year = 2015
+-- and cs.class_id = s.class_id
