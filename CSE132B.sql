7592a534a0e062b61bb772b6a5f2ffb1656950f0
Jenny Nguyen <ttn053@ucsd.edu>
2017-06-01 14:52:41 -0700

    SQL FILE

--- CSE132B.sql
+++ CSE132B.sql
@@ -1,3 +1,22 @@
+DROP table students_to_degrees;
+DROP table course_to_cat;
+DROP TABLE con_to_course;
+DROP table concentration;
+DROP TABLE students_to_degrees;
+DROP table degrees;
+DROP table prerequisites;
+DROP TABLE probations;
+DROP TABLE thesis_committee;
+DROP table enrollment;
+DROP TABLE meetings;
+DROP TABLE sections;
+DROP TABLE classes;
+DROP TABLE courses;
+DROP TABLE students;
+DROP table faculty;
+DROP table departments;
+DROP table grade_conversion
+
 CREATE TABLE departments(
   dept_id     SERIAL PRIMARY KEY,
   dept_name   TEXT NOT NULL UNIQUE,
@@ -91,35 +110,17 @@ CREATE TABLE probations(
 
 CREATE TABLE degrees(
   id          SERIAL PRIMARY KEY,
-  name        TEXT NOT NULL UNIQUE,
-  department  TEXT REFERENCES departments(dept_name),
-  type        TEXT NOT NULL
+  name        TEXT NOT NULL,
+  department  INTEGER REFERENCES departments(dept_id),
+  type        TEXT NOT NULL,
+  totalU      INTEGER NOT NULL check(totalU > 0),
+  lowerDiv    INTEGER NOT NULL check (lowerDiv  >= 0),
+  upperDiv    INTEGER NOT NULL check (upperDiv >= 0),
+  techElec    INTEGER check (techElec >= 0),
+  gradUnits   INTEGER check (gradUnits >= 0)
 );
 
---ADDED THIS RECENTLY
-CREATE TABLE students_to_degrees(
-  s_id        INTEGER REFERENCES students(id),
-  d_id        INTEGER REFERENCES degrees(id),
-  completed   TEXT NOT NULL
-);
 
-CREATE TABLE categories(
-  id          SERIAL PRIMARY KEY,
-  name        TEXT UNIQUE NOT NULL
-);
-
-CREATE TABLE requirements(
-  id          SERIAL PRIMARY KEY,
-  deg_id      INTEGER REFERENCES degrees(id),
-  cat_id      INTEGER REFERENCES categories(id),
-  units       INTEGER NOT NULL CHECK(units>0)
-);
-
-CREATE TABLE course_to_cat(
-  id          SERIAL PRIMARY KEY,
-  course      TEXT REFERENCES courses(course_name),
-  cat         INTEGER REFERENCES categories(id)
-);
 
 CREATE TABLE prerequisites(
   id          SERIAL PRIMARY KEY,
@@ -129,4 +130,68 @@ CREATE TABLE prerequisites(
 );
 
 
+CREATE TABLE concentration(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  minGPA REAL check (minGPA>=0.0),
+  units INTEGER check (units>=0),
+  did INTEGER references degrees(id)
+);
 
+CREATE TABLE con_to_course(
+  id      SERIAL PRIMARY KEY,
+  con       INTEGER REFERENCES concentration(id),
+  course      TEXT REFERENCES courses(course_name)
+);
+
+CREATE TABLE course_to_cat(
+  id          SERIAL PRIMARY KEY,
+  course      TEXT REFERENCES courses(course_name),
+  isLD        boolean,
+  isUD        boolean,
+  isTechE   boolean,
+  isGrad   boolean
+);
+
+
+CREATE TABLE students_to_degrees(
+  s_id INTEGER references students(id),
+  d_id INTEGER references degrees(id),
+  completed TEXT NOT NULL
+);
+
+
+create table GRADE_CONVERSION
+( LETTER_GRADE CHAR(2) NOT NULL,
+NUMBER_GRADE DECIMAL(2,1)
+);
+
+
+
+
+
+--OLD TABLE
+-- CREATE TABLE degrees(
+--   id          SERIAL PRIMARY KEY,
+--   name        TEXT NOT NULL UNIQUE,
+--   department  TEXT REFERENCES departments(dept_name),
+--   type        TEXT NOT NULL
+-- );
+
+-- CREATE TABLE categories(
+--   id          SERIAL PRIMARY KEY,
+--   name        TEXT UNIQUE NOT NULL
+-- );
+
+-- CREATE TABLE requirements(
+--   id          SERIAL PRIMARY KEY,
+--   deg_id      INTEGER REFERENCES degrees(id),
+--   cat_id      INTEGER REFERENCES categories(id),
+--   units       INTEGER NOT NULL CHECK(units>0)
+-- );
+
+-- CREATE TABLE course_to_cat(
+--   id          SERIAL PRIMARY KEY,
+--   course      TEXT REFERENCES courses(course_name),
+--   cat         INTEGER REFERENCES categories(id)
+-- );
